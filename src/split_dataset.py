90d54c3f4131501a1f5201e1b5b47699957bacbd
skevince <kai181216@outlook.com>
2021-03-06 21:18:41 +0800

    initial commit

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,18 +1,37 @@
 import os
 import random
 '''
+transform in into lowercase txt
+
+'''
+
+
+def lowercase_txt(file_name, new_file):
+    with open(file_name, 'r', encoding="utf8") as f:
+        contents = f.read()  # read contents of file
+    contents = contents.lower()  # convert to lower case
+    with open(new_file, 'w', encoding="utf8") as f:  # open for output
+        f.write(contents)
+
+
+'''
 randomly split the training set into 10 portions. 9 portions are for training, 
 and the other is for development, which will be used for early
 stopping or hyperparameter tuning.
 '''
-random.seed(1)
-ratio = 0.9
-with open('.././data/train_lc.txt', 'r') as f1:
-    lines = f1.readlines()
-random.shuffle(lines)
-lines_split = int(len(lines) * ratio)
-
-open('.././data/train.txt', 'w').write(''.join(lines[:lines_split]))
-open('.././data/dev.txt', 'w').write(''.join(lines[lines_split:]))
 
 
+def split(file_name, ratio):
+    random.seed(1)
+    ratio = ratio
+    with open(file_name, 'r') as f1:
+        lines = f1.readlines()
+    random.shuffle(lines)
+    lines_split = int(len(lines) * ratio)
+    open('.././data/train.txt', 'w').write(''.join(lines[:lines_split]))
+    open('.././data/dev.txt', 'w').write(''.join(lines[lines_split:]))
+
+
+if __name__ == '__main__':
+    lowercase_txt('.././data/train_5500.txt', '.././data/train_5500lowercase.txt')
+    split('.././data/train_5500lowercase.txt', 0.9)
