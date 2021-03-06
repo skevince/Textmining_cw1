b97da48fa8688460e602cb092aefdd8e2f32d0f8
skevince <kai181216@outlook.com>
2021-03-06 21:01:09 +0800

    initial commit

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,15 +1,19 @@
 import os
 import random
+'''
+randomly split the training set into 10 portions. 9 portions are for training, 
+and the other is for development, which will be used for early
+stopping or hyperparameter tuning.
+'''
+random.seed(1)
+ratio = 0.9
+with open('.././data/train_lc.txt', 'r') as f1:
+    lines = f1.readlines()
+print(type(lines))
+random.shuffle(lines)
+# lines = [line for line in lines if line.strip()]
+lines_split = int(len(lines) * ratio)
+open('.././data/train.txt', 'w').write(''.join(lines[:lines_split]))
+open('.././data/dev.txt', 'w').write(''.join(lines[lines_split:]))
 
-# preprocessing
-!curl https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label > ./data/train_5500.label
-!tr "[:upper:]" "[:lower:]" < ./data/train_5500.label > ./data/train_lc.txt
 
-
-
-
-
-
-#
-# if __name__ == "__main__":
-#     Main()
