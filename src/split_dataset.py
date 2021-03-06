85d09a02b0b941783e8beb2f90c6142230b1993b
skevince <kai181216@outlook.com>
2021-03-06 20:09:32 +0800

    initial commit

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -1,13 +1,15 @@
 import os
 import random
 
-def dev():
+# preprocessing
+!curl https://cogcomp.seas.upenn.edu/Data/QA/QC/train_5500.label > ./data/train_5500.label
+!tr "[:upper:]" "[:lower:]" < ./data/train_5500.label > ./data/train_lc.txt
 
 
 
 
 
 
-
-if __name__ == "__main__":
-    Main()
+#
+# if __name__ == "__main__":
+#     Main()
