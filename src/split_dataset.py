084e095c537ff22a9808c4baac8ce7884f52015f
skevince <kai181216@outlook.com>
2021-03-06 21:02:08 +0800

    initial commit

--- src/split_dataset.py
+++ src/split_dataset.py
@@ -9,10 +9,9 @@ random.seed(1)
 ratio = 0.9
 with open('.././data/train_lc.txt', 'r') as f1:
     lines = f1.readlines()
-print(type(lines))
 random.shuffle(lines)
-# lines = [line for line in lines if line.strip()]
 lines_split = int(len(lines) * ratio)
+
 open('.././data/train.txt', 'w').write(''.join(lines[:lines_split]))
 open('.././data/dev.txt', 'w').write(''.join(lines[lines_split:]))
 
