35f9eebc0ccdfae39d47712fe13621744437018b
skevince <kai181216@outlook.com>
2021-03-08 13:05:01 +0800

    initial commit

--- src/bow.py
+++ src/bow.py
@@ -5,7 +5,7 @@ import numpy
 def bow(embeddingmatrix):
     n = len(embeddingmatrix)
     bowvector = 0
-    for i in range (len(embeddingmatrix)):
+    for i in range(len(embeddingmatrix)):
         bowvector += embeddingmatrix[i]
 
     bowvector = bowvector /n
