b3432567ddd5ddb52fc7916c7c2785a3362a4872
skevince <kai181216@outlook.com>
2021-03-08 14:23:32 +0800

    initial commit

--- src/bilstm.py
+++ src/bilstm.py
@@ -5,15 +5,14 @@ import torch.nn.functional as F
 
 class BiLSTMTagger(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size):
-        super(LSTMTagger, self).__init__()
+        super(BiLSTMTagger, self).__init__()
         self.hidden_dim = hidden_dim
         self.word_embeddings = nn.Embedding(vocab_size, embedding_dim)
         # The BiLSTM takes word embeddings as inputs, and outputs hidden states
         # with dimensionality hidden_dim.
-        self .lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional = True )
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim, bidirectional=True)
         # The linear layer that maps from hidden state space to tag space
-        self .hidden2tag = nn.Linear(hidden_dim, tagset_size)
-
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
 
     def forward(self, *input):
         seq = input[0]
