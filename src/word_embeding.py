45c4ce4343871d94b605faf1239c1036552ef595
skevince <kai181216@outlook.com>
2021-03-08 22:46:07 +0800

    initial commit

--- src/word_embeding.py
+++ src/word_embeding.py
@@ -1,30 +1,30 @@
 import torch
 import re
+import numpy as np
 import random
 import torch.nn as nn
 
+np.set_printoptions(threshold=np.inf)
 
 def clean_stopwords(sentence):
     new_sentence = sentence
     stopwords = set()
-    with open('.././data/stopwords.txt', 'r') as f:
+    with open('D://Users\AW\Documents\GitHub\Textmining_cw1/data/stopwords.txt', 'r') as f:
         for stopword in f.readlines():
             stopword=stopword.rstrip("\n")
             stopwords.add(stopword)
     for word in sentence.split():
         if word in stopwords:
-            # print(word)
-            new_sentence = new_sentence.replace(word, "")
+            new_sentence=new_sentence.replace(word, '')
+            new_sentence = re.sub(r"\s+", " ", new_sentence)
         if word.isdigit():
-            new_sentence = new_sentence.replace(word, "")
-    new_sentence = re.sub(r"\s+", " ", new_sentence)
-
+            new_sentence=new_sentence.replace(word, '')
+            new_sentence = re.sub(r"\s+", " ", new_sentence)
     return new_sentence
 
 
 def get_voc(filepath):
-    vocabulary = {}
-    voc_set = []
+    vocabulary = []
     word_statistic = {}
     with open(filepath, 'r') as f:
         for line in f.readlines():
@@ -34,31 +34,37 @@ def get_voc(filepath):
                     word_statistic[word] += 1
                 else:
                     word_statistic[word] = 1
-            for word in word_statistic:
-                if word_statistic[word] > 3:
-                    voc_set.append(word)
-    for i in range(len(voc_set)):
-        vocabulary[voc_set[i]] = 0
-    print(vocabulary)
+    for word in word_statistic:
+        if word_statistic[word] > 2:
+            vocabulary.append(word)
     return vocabulary
 
 
 def get_vector(filepath, vocabulary):
+    word_list=[]
     vector = []
     with open(filepath, 'r') as f:
         for line in f.readlines():
-            sentence = []
-            void_stopwords = clean_stopwords(line.split(" ", 1)[1])
+            void_stopwords = line.split(" ", 1)[1]
+            pre_vec=np.zeros(len(vocabulary))
             for word in void_stopwords.split():
-                print(word)
-                sentence.append(vocabulary[word])
-            vector.append(sentence)
-    return vector
+                if word in vocabulary:
+                    pre_vec[vocabulary.index(word)]=pre_vec[vocabulary.index(word)]+1
+            for word in void_stopwords.split():
+                vec_temp=pre_vec
+                if word in vocabulary:
+                    vec_temp[vocabulary.index(word)]=0
+                if word not in word_list:
+                    word_list.append(word)
+                    vector.append(vec_temp)
+                else:
+                    vector[word_list.index(word)]=vector[word_list.index(word)]+vec_temp
+    return word_list,vector
 
 
 def randomly_embedding(filepath, dimension):
     vocabulary = get_voc(filepath)
-    vector = get_vector(filepath, vocabulary)
+    word_list,vector = get_vector(filepath, vocabulary)
     torch.manual_seed(1)
     embedding = nn.Embedding(len(vocabulary), dimension)
     label = torch.LongTensor(vector)
@@ -68,5 +74,4 @@ def randomly_embedding(filepath, dimension):
 
 if __name__ == '__main__':
     print("Randomly initialised word embeddings")
-    initial_embedding = randomly_embedding('.././data/train_1.txt', 1000)
-
+    initial_embedding = randomly_embedding('.././data/train.txt', 1000)
